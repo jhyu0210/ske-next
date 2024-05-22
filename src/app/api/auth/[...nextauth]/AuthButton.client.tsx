fe989096947a7a1854567debcefa9c4fac136179
james <jhyu0210@gmail.com>
2024-05-22 11:48:15 +0900

    delete unused components

--- src/app/api/auth/[...nextauth]/AuthButton.client.tsx
+++ src/app/api/auth/[...nextauth]/AuthButton.client.tsx
@@ -1,29 +1,29 @@
-"use client";
-import { Button } from "~/components/ui/button";
-import { useSession } from "next-auth/react";
-import { signIn, signOut } from "~/auth/helpers";
-function AuthButtonClient() {
-  const session = useSession();
-  return session?.data?.user ? (
-    <Button
-      className="rounded-lg border border-slate-200"
-      variant="ghost"
-      onClick={async () => {
-        await signOut();
-        await signIn();
-      }}
-    >
-      {session?.data?.user.name}:Sign Out
-    </Button>
-  ) : (
-    <Button
-      className="rounded-lg border border-slate-200"
-      variant="ghost"
-      onClick={async () => await signIn()}
-    >
-      Sign In
-    </Button>
-  );
-}
+// "use client";
+// import { Button } from "~/components/ui/button";
+// import { useSession } from "next-auth/react";
+// import { signIn, signOut } from "~/auth/helpers";
+// function AuthButtonClient() {
+//   const session = useSession();
+//   return session?.data?.user ? (
+//     <Button
+//       className="rounded-lg border border-slate-200"
+//       variant="ghost"
+//       onClick={async () => {
+//         await signOut();
+//         await signIn();
+//       }}
+//     >
+//       {session?.data?.user.name}:Sign Out
+//     </Button>
+//   ) : (
+//     <Button
+//       className="rounded-lg border border-slate-200"
+//       variant="ghost"
+//       onClick={async () => await signIn()}
+//     >
+//       Sign In
+//     </Button>
+//   );
+// }
 
-export default AuthButtonClient;
+// export default AuthButtonClient;
