edc9b0ec9ed33db95d06950aa4f71bac5252dbd9
james <jhyu0210@gmail.com>
2024-05-25 22:56:59 +0900

    adding resend address onboarding@resend.dev

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -76,9 +76,9 @@ export async function POST(req: Request) {
       );
 
       await resend.emails.send({
-        from: "SKE-NEXT <jhyu0210@gmail.com>",
+        from: "onboarding@resend.dev",
         to: [event.data.object.customer_details.email],
-        subject: "Thanks for your order!",
+        subject: "Thanks for your order!!!",
         react: OrderReceivedEmail({
           orderId,
           orderDate: updatedOrder.createdAt.toLocaleDateString(),
